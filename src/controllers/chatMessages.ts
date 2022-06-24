309923ec5ef34f15c391b5093cad86cd70d617e0
Ben Kremer <contact@bkrem.dev>
2022-06-24 12:01:48 +0200

    chore: adds note on StoreStruct typing

--- src/controllers/chatMessages.ts
+++ src/controllers/chatMessages.ts
@@ -6,6 +6,8 @@ import {
   CHAT_MESSAGES_CONTEXT,
 } from "../constants";
 
+// FIXME: `StoreStruct` is opinionated towards SignClient data types -> make it agnostic.
+// StoreStruct = SessionTypes.Struct | PairingTypes.Struct | ProposalTypes.Struct;
 export class ChatMessages extends Store<string, any> {
   constructor(public core: ICore, public logger: Logger) {
     super(core, logger, CHAT_MESSAGES_CONTEXT, CHAT_CLIENT_STORAGE_PREFIX);
