5ac554049b36cd15147bf01a6c4451ae100d06f7
Gancho Radkov <ganchoradkov93@gmail.com>
2022-10-26 22:53:27 +0300

    fix: correctly persists and repopulates pending chat invites & public keys

--- src/controllers/chatKeys.ts
+++ src/controllers/chatKeys.ts
@@ -2,11 +2,20 @@ import { Logger } from "pino";
 import { Store } from "@walletconnect/core";
 import { ICore } from "@walletconnect/types";
 import { CHAT_CLIENT_STORAGE_PREFIX } from "../constants";
+import { ChatClientTypes } from "../types";
 
 // FIXME: `StoreStruct` is opinionated towards SignClient data types -> make it agnostic.
 // StoreStruct = SessionTypes.Struct | PairingTypes.Struct | ProposalTypes.Struct;
-export class ChatKeys extends Store<string, any> {
+export class ChatKeys extends Store<string, ChatClientTypes.ChatKey> {
   constructor(public core: ICore, public logger: Logger) {
-    super(core, logger, "chat_keys", CHAT_CLIENT_STORAGE_PREFIX);
+    super(
+      core,
+      logger,
+      "chat_keys",
+      CHAT_CLIENT_STORAGE_PREFIX,
+      (chats: ChatClientTypes.ChatKey) => {
+        return chats._key;
+      }
+    );
   }
 }
