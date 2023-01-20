376d108373b9b29ddbfedca461dff6dfdea1c6cc
Ben Kremer <contact@bkrem.dev>
2023-01-20 16:58:29 +0700

    fix(tests): throw early if `TEST_PROJECT_ID` is missing

--- packages/chat-client/test/client/client.spec.ts
+++ packages/chat-client/test/client/client.spec.ts
@@ -4,6 +4,10 @@ import ChatClient from "../../src";
 import { ChatClientTypes } from "../../src/types";
 import { disconnectSocket } from "../helpers/ws";
 
+if (!process.env.TEST_PROJECT_ID) {
+  throw new ReferenceError("TEST_PROJECT_ID env var not set");
+}
+
 const TEST_CLIENT_ACCOUNT =
   "eip155:1:0xf07A0e1454771826472AE22A212575296f309c8C";
 const TEST_PEER_ACCOUNT = "eip155:1:0xb09a878797c4406085fA7108A3b84bbed3b5881F";
